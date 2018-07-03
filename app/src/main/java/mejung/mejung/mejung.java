a5ca4c73f1be4742dd243b72d2cc8f86f18e628b
jongh0916 <jongh999@gmail.com>
2018-07-03 16:24:33 +0900

    first commit

---
+++ app/src/main/java/mejung/mejung/mejung.java
@@ -0,0 +1,13 @@
+package mejung.mejung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mejung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.project_mejung);
+    }
+}
